7f849ad4d47f82030f3de3f69222b26e077b0d6c
sigrundish <sigrun@dohop.com>
2018-04-03 16:05:36 +0200

    also pass user data when login

--- db/loginQueries.js
+++ db/loginQueries.js
@@ -5,12 +5,18 @@ function login(req, res, next) {
   db
     .any(
       'select username,password from users where $1 = username AND $2 = password',
-      [body.username, body.password],
+      [body.username, body.password]
     )
     .then(function(data) {
       if (data.length > 0) {
         res.status(200).json({
           login: true,
+          data: data[0],
+        });
+      } else {
+        res.status(200).json({
+          login: false,
+          data: [],
         });
       }
     })

5a9810d0270aaf203b03793663966343fb683933
sigrundish <sigrun@dohop.com>
2018-04-04 23:10:01 +0000

    refactor login

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,37 @@
 var express = require('express');
 var router = express.Router();
-const { login } = require('../db/loginQueries');
+const {
+  findByUsername,
+  findByUsernameAndPassword,
+} = require('../db/usersQueries');
+
+async function login(req, res, next) {
+  const { username, password } = req.body;
+  const userByUsername = await findByUsername(username);
+  const userByUsernameAndPw = await findByUsernameAndPassword({
+    username,
+    password,
+  });
+  if (userByUsernameAndPw) {
+    res.status(200).json({
+      login: true,
+      data: userByUsernameAndPw,
+      validation: [],
+    });
+  } else if (userByUsername) {
+    res.status(200).json({
+      login: false,
+      data: userByUsername,
+      validation: [{ error: 'Wrong password.' }],
+    });
+  } else {
+    res.status(200).json({
+      login: false,
+      data: {},
+      validation: [{ error: 'Username not found.' }],
+    });
+  }
+}
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {

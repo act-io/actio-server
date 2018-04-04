fa9b73157f7927562125763a8001d4ef9925c8ba
Markús Freyr <markusfreyr@Markuss-MacBook-Pro.local>
2018-04-04 19:56:59 +0000

    make it better

--- db/usersAttendingActivity.js
+++ db/usersAttendingActivity.js
@@ -93,7 +93,7 @@ async function deleteFromUsersAttendingActivity(userId, activityId) {
 function getHowManyAttendees(req, res, next) {
   const { activityID } = req.params;
   db 
-    .any('SELECT COUNT(*) FROM usersAttendingActivity WHERE activityId = $1',[xss(activityID)])
+    .any('SELECT COUNT(*)::integer FROM usersAttendingActivity WHERE activityId = $1',[xss(activityID)])
     .then(function(data) {
       res.status(200).json({
         status: 'success',

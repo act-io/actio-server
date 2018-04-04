64657e81780b9431a1c28a5204e239d28fcd18d8
sigrundish <sigrun@dohop.com>
2018-04-04 16:09:41 +0000

    change back

--- schema.sql
+++ schema.sql
@@ -1,4 +1,17 @@
+CREATE TABLE users (
+  id serial PRIMARY KEY,
+  name text NOT NULL,
+  age text NOT NULL,
+  username character varying(50) NOT NULL,
+  password character varying(60) NOT NULL 
+);
 
+CREATE TABLE activities (
+  id serial PRIMARY KEY,
+  title character varying(50) NOT NULL,
+  description text NOT NULL, 
+  location character varying(50) NOT NULL
+);
 
 CREATE TABLE usersAttendingActivity (
   userId integer NOT NULL,
